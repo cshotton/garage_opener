25e0a30d10c8411d5b344b70a6e21d1dacdbed41
cshotton <chuck@shotton.com>
2018-01-27 13:04:27 -0500

    changed to onoff library for gpio

--- routes/door.js
+++ routes/door.js
@@ -1,35 +1,23 @@
-var raspi = require ('raspi');
-var gpio = require('raspi-gpio');
+var gpio = require('onoff').Gpio;
 const appRoot = require('app-root-path');
 
 var express = require('express');
 var router = express.Router();
 
 var lastDate = 0;
-var pin17 = 'GPIO17';
-var button = null;
+var pin = 22;
+var relay = null;
 
 //init
-raspi.init (() => {
-        try {
-            button = new gpio.DigitalOutput({
-                    pin : pin17,
-                    pullResistor: gpio.PULL_UP
-                });
-            button.write (gpio.HIGH);
-        }
-        catch (err) {
-            console.log ("gpio err " + err);
-        }
-    });
+relay = new gpio(pin,'high');
     
 function pushButton (val) {
     if (val) {
-        button.write (gpio.LOW);
+        relay.writeSync (0);
         setTimeout (pushButton, 500, false);
     }
     else {
-        button.write (gpio.HIGH);
+        relay.writeSync (1);
     }
 }
 
