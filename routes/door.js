1cedf5eae3d0a0142159853056d6c52e44aa2c5d
cshotton <chuck@shotton.com>
2018-01-31 20:38:31 -0500

    adding support for magnetic door sensors

--- routes/door.js
+++ routes/door.js
@@ -5,12 +5,54 @@ var express = require('express');
 var router = express.Router();
 
 var lastDate = 0;
-var pin = 22;
+var rpin = 22;
+var opin = 17;
+var cpin = 27;
+
 var relay = null;
+var oswitch = null;
+var cswitch = null;
+
+var cstate = 0;
+var ostate = 0;
 
 //init
-relay = new gpio(pin,'high');
-    
+relay = new gpio(rpin,'high');
+
+function makeCallback (pin) {
+    return function (err, value) {
+        var thePin = pin;
+        var tstate = 99;
+        
+        switch (thePin) {
+            case opin:
+                if (value != ostate) {
+                    ostate = value;
+                    tstate = ostate;
+                }
+                break;
+                
+            case cpin:
+                if (value != cstate) {
+                    cstate = value;
+                    tstate = cstate;
+                }
+                break;
+        }
+        
+        if (tstate != 99)
+            console.log ('Pin ' + thePin + ': ' + tstate);
+        if (err) {
+            console.log (err);
+        }
+    };
+}
+
+oswitch = new gpio (opin, 'in', 'both');
+cswitch = new gpio (cpin, 'in', 'both');
+oswitch.watch (makeCallback (opin));
+cswitch.watch (makeCallback (cpin));
+
 function pushButton (val) {
     if (val) {
         relay.writeSync (0);
@@ -38,5 +80,14 @@ router.get('/status', function(req, res, next) {
     var obj = {status:'ok', msg:'Door last opened', date: getDate()};
     res.send(obj);
 });
+
+function exitHandler (options, err) {
+    console.log ('(Ctrl-C) terminating.');
+    gpio.unwatchAll();
+    process.exit();
+}
+
+process.on( 'SIGINT', exitHandler.bind (null, {exit:true}));
+
 
 module.exports = router;
